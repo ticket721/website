3dd266cb440fb24a7ad7cc26fc76d6f4051623ed
Jérémy Goldsztejn <jgoldsztejn@advanced-schema.com>
2019-06-20 19:07:18 +0200

    Fix lists shift issue

--- src/sections/description/index.js
+++ src/sections/description/index.js
@@ -6,125 +6,80 @@ import coins from '../../assets/coins.png';
 import locked_wallet from '../../assets/locked_wallet.png';
 import marketplace_cart from '../../assets/marketplace_cart.png';
 import configure from '../../assets/configure.png';
-import store_revenue from '../../assets/store_revenue.png'
-import stats from '../../assets/stats.png'
+import store_revenue from '../../assets/store_revenue.png';
+import stats from '../../assets/stats.png';
+import './separator.css';
 
 class Description extends React.Component {
 
     render() {
+        const userData = [];
+        const organizerData = [];
 
-        const data = [
-            <div
-                style={{
-                    height: '100%',
-                    textAlign: 'none',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}
-            >
-                <div style={{marginBottom: 36}}>
-                    <Typography.Text style={{fontSize: 36, color: '#F0F2F4', marginBottom: 24}}>
-                        {this.props.t('description_user_title')}
-                    </Typography.Text>
-                </div>
+        const metadata = {
+            user: [
+                {
+                    icon: coins,
+                    alt: 'Coins'
+                },
+                {
+                    icon: locked_wallet,
+                    alt: 'Wallet'
+                },
+                {
+                    icon: marketplace_cart,
+                    alt: 'Marketplace'
+                }
+            ],
+            organizer: [
+                {
+                    icon: configure,
+                    alt: 'Configuring'
+                },
+                {
+                    icon: store_revenue,
+                    alt: 'Revenue'
+                },
+                {
+                    icon: stats,
+                    alt: 'Stats'
+                }
+            ]
+        };
+
+        for (let i = 0; i < 3; i++) {
+            userData.push(
                 <div style={{
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
-                    justifyContent: 'center',
-                    marginBottom: 24,
+                    justifyContent: 'space-between',
+                    marginBottom: 15,
                     width: '100%'
                 }}>
-                    <img style={{width: '50%'}} src={coins} alt={'Coins'}/>
-                    <Typography.Text style={{fontSize: 26, color: '#F0F2F4', fontWeight: 300, margin: 12}}>
-                        {this.props.t('description_user_1')}
+                    <img style={{width: '50%'}} src={metadata.user[i].icon} alt={metadata.user[i].alt}/>
+                    <Typography.Text style={{fontSize: '1.5em', color: '#F0F2F4', fontWeight: 300, margin: 12}}>
+                        {this.props.t(`description_user_${i + 1}`)}
                     </Typography.Text>
                 </div>
+            )
+
+            organizerData.push(
                 <div style={{
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
-                    justifyContent: 'center',
-                    marginBottom: 24,
-                    width: '100%'
+                    justifyContent: 'space-between',
+                    marginBottom: 15,
+                    width: '100%',
                 }}>
-                    <img style={{width: '50%'}} src={locked_wallet} alt={'Wallet'}/>
-                    <Typography.Text style={{fontSize: 26, color: '#F0F2F4', fontWeight: 300, margin: 12}}>
-                        {this.props.t('description_user_2')}
+                    <img style={{width: '50%'}} src={metadata.organizer[i].icon} alt={metadata.organizer[i].alt}/>
+                    <Typography.Text style={{fontSize: '1.5em', color: '#F0F2F4', fontWeight: 300, margin: 12}}>
+                        {this.props.t(`description_organizer_${i + 1}`)}
                     </Typography.Text>
                 </div>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    marginBottom: 24,
-                    width: '100%'
-                }}>
-                    <img style={{width: '50%'}} src={marketplace_cart} alt={'Marketplace'}/>
-                    <Typography.Text style={{fontSize: 26, color: '#F0F2F4', fontWeight: 300, margin: 12}}>
-                        {this.props.t('description_user_3')}
-                    </Typography.Text>
-                </div>
-            </div>,
-            <div
-                style={{
-                    height: '100%',
-                    textAlign: 'none',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}
-            >
-                <div style={{marginBottom: 36}}>
-                    <Typography.Text style={{fontSize: 36, color: '#F0F2F4', marginBottom: 24}}>
-                        {this.props.t('description_organizer_title')}
-                    </Typography.Text>
-                </div>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    marginBottom: 24,
-                    width: '100%'
-                }}>
-                    <img style={{width: '50%'}} src={configure} alt={'Configuring'}/>
-                    <Typography.Text style={{fontSize: 26, color: '#F0F2F4', fontWeight: 300, margin: 12}}>
-                        {this.props.t('description_organizer_1')}
-                    </Typography.Text>
-                </div>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    marginBottom: 24,
-                    width: '100%'
-                }}>
-                    <img style={{width: '50%'}} src={store_revenue} alt={'Revenue'}/>
-                    <Typography.Text style={{fontSize: 26, color: '#F0F2F4', fontWeight: 300, margin: 12}}>
-                        {this.props.t('description_organizer_2')}
-                    </Typography.Text>
-                </div>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    marginBottom: 24,
-                    width: '100%'
-                }}>
-                    <img style={{width: '50%'}} src={stats} alt={'Stats'}/>
-                    <Typography.Text style={{fontSize: 26, color: '#F0F2F4', fontWeight: 300, margin: 12}}>
-                        {this.props.t('description_organizer_3')}
-                    </Typography.Text>
-                </div>
-            </div>
-        ];
+            )
+        }
 
         return <div
             style={{
@@ -139,7 +94,7 @@ class Description extends React.Component {
                 style={{
                     width: '100%',
                     padding: 25,
-                    paddingTop: 100,
+                    paddingTop: 150,
                     paddingBottom: 100,
                     backgroundImage: `linear-gradient(135deg, #212121 30%, transparent 30%, transparent 70%, #212121 30%)`,
                     display: 'flex',
@@ -170,25 +125,67 @@ class Description extends React.Component {
                 >
                     {this.props.t('slogan_body')}
                 </Typography.Text>
-                <List
-                    style={{width: '70%', marginTop: 100}}
-                    grid={{
-                        xs: 1,
-                        sm: 1,
-                        md: 1,
-                        lg: 2,
-                        xl: 2,
-                        xxl: 2,
+                <div
+                    className='user-organizer-container'
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-around',
+                        width: '100%',
+                        boxSizing: 'border-box',
+                        marginTop: 150
                     }}
-                    dataSource={data}
-                    renderItem={item => (
-                        <List.Item>
-                            {item}
-                        </List.Item>
-                    )
-                    }
-                />
-
+                >
+                    <List
+                        style={{
+                            flex: '1 auto',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            paddingTop: 10
+                        }}
+                        header={
+                            <div style={{marginBottom: 36}}>
+                                <Typography.Text style={{fontSize: 36, color: '#F0F2F4', marginBottom: 24, width: '80%'}}>
+                                    {this.props.t('description_user_title')}
+                                </Typography.Text>
+                            </div>
+                        }
+                        dataSource={userData}
+                        renderItem={item => (
+                            <List.Item>
+                                {item}
+                            </List.Item>
+                        )
+                        }
+                    />
+                    <div className='separator'/>
+                    <List
+                        style={{
+                            flex: '1 auto',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            paddingTop: 10,
+                        }}
+                        header={
+                            <div style={{marginBottom: 36}}>
+                                <Typography.Text style={{fontSize: 36, color: '#F0F2F4', marginBottom: 24, width: '80%'}}>
+                                    {this.props.t('description_organizer_title')}
+                                </Typography.Text>
+                            </div>
+                        }
+                        dataSource={organizerData}
+                        renderItem={item => (
+                            <List.Item>
+                                {item}
+                            </List.Item>
+                        )
+                        }
+                    />
+                </div>
             </div>
         </div>;
     }
