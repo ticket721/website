4668a73b49c11f3d58097660b5d074f868a0513e
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-06-24 22:51:17 +0200

    fix: assets and footer refactor
      - Grayscaled assets and simpler footer

--- src/sections/footer/index.js
+++ src/sections/footer/index.js
@@ -1,11 +1,10 @@
-import { Typography } from 'antd';
 import React from 'react';
 import { withTranslation } from 'react-i18next';
 import logo from '../../assets/logo.svg'
-import slack from '../../assets/slack.png';
-import telegram from '../../assets/telegram.png';
-import spectrum from '../../assets/spectrum.png';
-import mail from '../../assets/mail.png';
+import slack from '../../assets/slack.svg';
+import telegram from '../../assets/telegram.svg';
+import spectrum from '../../assets/spectrum.svg';
+import mail from '../../assets/mail.svg';
 import './index.css';
 
 class Footer extends React.Component {
@@ -14,38 +13,32 @@ class Footer extends React.Component {
             style={{
                 width: '100%',
                 display: 'flex',
-                flexDirection: 'column',
                 alignItems: 'center',
                 justifyContent: 'center',
-                padding: 24
+                padding: 6
             }}
         >
-            <div>
-                <Typography.Text style={{fontSize: 22, fontWeight: 300}}>{this.props.t('chat_with_us')}</Typography.Text>
-            </div>
             <div
                 style={{
                     width: '100%',
                     display: 'flex',
                     alignItems: 'center',
-                    justifyContent: 'center',
+                    justifyContent: 'flex-end',
                 }}
             >
+                <img alt={''} src={logo} style={{width: 40, margin: 12}}/>
+                <a href={'mailto:contact@ticket721.com'}>
+                    <img alt={'Mail'} src={mail} className={'contact_logo'} style={{width: 30}}/>
+                </a>
                 <a href={'https://join.slack.com/t/ticket721/shared_invite/enQtNjY1MTMyNzcxNTM3LTQ5YTM5ODg2NzNkNDg3ODgxNzk0ZTg3Nzg2YWY3ZjQ4NWFkZWVhNzYyMDk0NGZkY2RiZGM4OTMzNzcyZmZjODU'} target={'_blank'}>
-                    <img alt={'Slack'} src={slack} className={'contact_logo'}/>
+                    <img alt={'Slack'} src={slack} className={'contact_logo'} style={{width: 30}}/>
                 </a>
                 <a href={'https://t.me/joinchat/GAJnuxcNZ3tcL0rsPT4OxA'} target={'_blank'}>
-                    <img alt={'Telegram'} src={telegram} className={'contact_logo'}/>
+                    <img alt={'Telegram'} src={telegram} className={'contact_logo'} style={{width: 30}}/>
                 </a>
                 <a href={'https://spectrum.chat/ticket721'} target={'_blank'}>
-                    <img alt={'Spectrum'} src={spectrum} className={'contact_logo'}/>
+                    <img alt={'Spectrum'} src={spectrum} className={'contact_logo'} style={{width: 30}}/>
                 </a>
-                <a href={'mailto:contact@ticket721.com'}>
-                    <img alt={'Mail'} src={mail} className={'contact_logo'}/>
-                </a>
-            </div>
-            <div style={{marginTop: 24}}>
-                <img alt={''} src={logo} style={{width: 50}}/>
             </div>
         </div>;
     }
